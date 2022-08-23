893a3df3d7c4c00989d5c1f1d511bd132c71c6ea
Rene0115 <enere0115@gmail.com>
2022-08-23 11:00:30 +0100

    user model fix

---
+++ models/event.model.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const eventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  tagline: {
+    type: String,
+    required: true
+  },
+  schedule:{
+    type: String,
+    required: true
+  }
+
+} { timestamps: true});
