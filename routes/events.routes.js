34b609a6d2dd5c900c4e0167edf50455598e0545
Rene0115 <enere0115@gmail.com>
2022-08-23 11:00:15 +0100

    user model fix

---
+++ routes/events.routes.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import validateEventSchema from '../validators/events.validator.js';
+
+const eventRouter = express.Router();
+
+export default eventRouter;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,5 +1,10 @@
+/* eslint-disable import/extensions */
 import express from 'express';
+import userRouter from './user.routes.js';
+import eventRouter from './event.routes.js';
 
 const router = express.Router();
+router.use('/users', userRouter);
+router.use('/events', eventRouter);
 
 export default router;
